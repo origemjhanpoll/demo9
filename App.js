cfc051a935e631406dfe1d5bf24782539dd00ec4
Jean Paul | Origem <jeanpaul2022@gmail.com>
2021-01-01 17:27:59 -0300

    add animation

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Animated, Pressable, Vibration } from "react-native";
+import { PanGestureHandler } from "react-native-gesture-handler";
 
 export default function App() {
+  const ref = useRef(new Animated.Value(1)).current;
+
+  const animaScale = () => {
+    Vibration.vibrate(100)
+    Animated.sequence([
+      Animated.timing(ref, {
+        toValue: 1.1,
+        duration: 80,
+        useNativeDriver: true,
+      }),
+      Animated.timing(ref, {
+        toValue: 1,
+        duration: 80,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable onPress={animaScale}>
+        <Animated.View style={[styles.circle, { transform: [{ scale: ref }] }]} />
+      </Pressable>
     </View>
   );
 }
@@ -14,8 +34,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  circle: {
+    width: 150,
+    height: 150,
+    backgroundColor: "green",
+    borderRadius: 100,
   },
 });
